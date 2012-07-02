4d6b3cc0516a09b52263e3f0a936cd3eb31e1a4f
Ryan Hope <rmh3093@gmail.com>
2012-07-02 13:47:11 -0400

    Rewritten as original code did not work with my test EDF+ file. The main change is the removal of the regex module to use the struct module instead.

--- pyedf.py
+++ pyedf.py
@@ -1,64 +1,75 @@
 #!/usr/bin/env python
 # encoding: utf-8
+
 """
 pyedf.py
 
 Created by Ray Slakinski on 2010-09-14.
 Copyright (c) 2010 Ray Slakinski. All rights reserved.
+
+Modified by Ryan M. Hope on 2012-07-01.
+Copyright (c) 2012 Ryan M. Hope. All rights reserved.
+
+EDF/EDF+ Spec:
+    http://www.edfplus.info/specs/index.html
 """
+
 import sys
 import argparse
-import re
+import struct
+from pprint import pprint
 
+def parse_edf_file( fileobj ):
 
-def read_edf_file(fileobj):
-    data = fileobj.read()
+    header_raw = map( str.strip, struct.unpack_from( '8s80s80s8s8s8s44s8s8s4s', fileobj.read( 256 ) ) )
+
     header = {}
-    # Parse header information based on the EDF/EDF+ specs
-    # http://www.edfplus.info/specs/index.html
-    header['version'] = data[0:7].strip()
-    header['patient_id'] = data[7:88].strip()
-    header['rec_id'] = data[88:168].strip()
-    header['startdate'] = data[168:176].strip()
-    header['starttime'] = data[176:184].strip()
-    header['header_bytes'] = int(data[184:192].strip())
-    header['num_items'] = int(data[236:244].strip())
-    header['data_duration'] = float(data[244:252].strip())
-    header['num_signals'] = int(data[252:256].strip())
-    # more data 256 chars down. in header, but ignoring for now
-    records = []
-    records_a = records.append
-    rec_pos = 512
-    rec_size = 36
-    # skip first rec
-    rec_pos += rec_size
-    for i in range(header['num_items']-1):
-        record = {}
-        record_split = data[rec_pos:rec_pos+rec_size].split('\x14')
-        matches = re.findall(r'([\d]+)', record_split[2])
-        record['type'] = record_split[3].strip()
-        record['time'] = {
-            'start': int(matches[0]),
-            'durration': int(matches[1]),
-        }
-        records_a(record)
-        rec_pos += rec_size
-    return {'header': header, 'records': records}
+    header['version'] = int( header_raw[0] )
+    header['patient_id'] = header_raw[1]
+    header['rec_id'] = header_raw[2]
+    header['startdate'] = header_raw[3]
+    header['starttime'] = header_raw[4]
+    header['header_bytes'] = int( header_raw[5] )
+    header['reserved'] = header_raw[6]
+    header['num_records'] = int( header_raw[7] )
+    header['data_duration'] = float( header_raw[8] )
+    header['num_signals'] = int( header_raw[9] )
 
+    signals = {}
+    signals['labels'] = map( str.strip, struct.unpack_from( '16s' * header['num_signals'], fileobj.read( 16 * header['num_signals'] ) ) )
+    signals['type'] = map( str.strip, struct.unpack_from( '80s' * header['num_signals'], fileobj.read( 80 * header['num_signals'] ) ) )
+    signals['physical_dim'] = map( str.strip, struct.unpack_from( '8s' * header['num_signals'], fileobj.read( 8 * header['num_signals'] ) ) )
+    signals['physical_min'] = map( int, struct.unpack_from( '8s' * header['num_signals'], fileobj.read( 8 * header['num_signals'] ) ) )
+    signals['physical_max'] = map( int, struct.unpack_from( '8s' * header['num_signals'], fileobj.read( 8 * header['num_signals'] ) ) )
+    signals['digital_min'] = map( int, struct.unpack_from( '8s' * header['num_signals'], fileobj.read( 8 * header['num_signals'] ) ) )
+    signals['digital_max'] = map( int, struct.unpack_from( '8s' * header['num_signals'], fileobj.read( 8 * header['num_signals'] ) ) )
+    signals['pre_filtering'] = map( str.strip, struct.unpack_from( '80s' * header['num_signals'], fileobj.read( 80 * header['num_signals'] ) ) )
+    signals['num_samples'] = map( int, struct.unpack_from( '8s' * header['num_signals'], fileobj.read( 8 * header['num_signals'] ) ) )
+    signals['reserved'] = map( str.strip, struct.unpack_from( '32s' * header['num_signals'], fileobj.read( 32 * header['num_signals'] ) ) )
+
+    data = {}
+    for signal in signals['labels']:
+        data[signal] = []
+    for r in range( 0, header['num_records'] ):
+        for s, signal in enumerate( signals['labels'] ):
+            data[signal].append( map( int, struct.unpack_from( '%sh' % signals['num_samples'][s], fileobj.read( 2 * signals['num_samples'][s] ) ) ) )
+
+    return {'header': header, 'signals': signals, 'data': data}
 
 def main():
-    # create the parser
-    parser = argparse.ArgumentParser(description='Process a given EDF File.')
+    """This is really only usefull for testing purposes."""
+    parser = argparse.ArgumentParser( description = 'Process a given EDF File.' )
     parser.add_argument(
         '-f',
         '--file',
-        type=argparse.FileType('r'),
-        help='EDF File to be processed.',
+        type = argparse.FileType( 'r' ),
+        required = True,
+        help = 'EDF File to be processed.',
     )
     args = parser.parse_args()
-    data = read_edf_file(args.file)
+    data = parse_edf_file( args.file )
+    pprint( data['header'] )
     args.file.close()
-    print data
 
 if __name__ == '__main__':
     main()
